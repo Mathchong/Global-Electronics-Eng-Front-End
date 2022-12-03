a1b7c3b55ab4929f4a066dfcaef247c85c27d2f3
Matheus Chong <matheus.chong@gmail.com>
2022-12-03 08:22:31 -0300

    Chore: Created login page

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 
+import Login from "../pages/login";
 
 export default function App() {
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path='/sing-up' element={<h1>Tela de Cadastro</h1>} />
-      <Route path='*' element={<h1>Desculpe, não entramos a página</h1>}/>
-    </Routes>
+    <BrowserRouter> 
+      <Routes>
+        <Route path="/sing-up" element={<h1>Tela de Cadastro</h1>} />
+        <Route path="/sign-in" element={<Login/>}/>
+        <Route path="*" element={<h1>Desculpe, não entramos a página</h1>} />
+      </Routes>
     </BrowserRouter>
   );
 }
---
+++ src/pages/login.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default function Login() {
+  return <Title>Login Page</Title>;
+}
+
+const Title = styled.h1`
+  font-size: 30px;
+`;
